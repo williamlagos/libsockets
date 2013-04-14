23c7708574cb9f17f94f3c23e7d3b08eaedb9c9a
William Oliveira de Lagos <efforiaca@gmail.com>
2013-04-14 15:09:29 -0300

    Added debug mode and reestructured client module

--- Makefile
+++ Makefile
@@ -3,3 +3,7 @@ CC = gcc
 all:
 	$(CC) main.c -o main
 	$(CC) client.c -o client
+
+debug:
+	$(CC) main.c -o main -g
+	$(CC) client.c -o client -g
--- client.c
+++ client.c
@@ -1,37 +1,39 @@
 #include "sockets.h"
 
-void 
-create_socket
-(int* socket_file)
+int 
+create_socket()
 {
-	int fd = *socket_file;
-	fd = socket(AF_INET,SOCK_STREAM,0);
+	int fd = socket(AF_INET,SOCK_STREAM,0);
 	if(fd < 0) error("ERRO ao tentar abrir o soquete");
-	else socket_file = (int*) fd;
+	else return fd;
 }
 
-void 
+struct
+sockaddr_in
 build_address
-(struct sockaddr_in* server_addr,
- struct hostent* server,
+(const char* host_name,
  int port_number)
 {
-	bzero((char*) server_addr, sizeof(*server_addr));
-	server_addr->sin_family = AF_INET;
-	bcopy((char*) server->h_addr,
-	      (char*) server_addr->sin_addr.s_addr,
-	      server->h_length);
-	server_addr->sin_port = htons(port_number);
+	struct sockaddr_in serv_addr;
+	struct hostent *server = gethostbyname(host_name);
+	if(server == NULL) error("ERRO, nenhum host encontrado\n");
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr, 
+		(char *)&serv_addr.sin_addr.s_addr,
+		server->h_length);
+	serv_addr.sin_port = htons(port_number);
+	return serv_addr;
 }
 
 void
 connect_socket
 (int* socket_file,
- struct sockaddr_in* server_addr)
+ struct sockaddr_in server_addr)
 {
 	int status,fd;
 	fd = *socket_file;
-	status = connect(fd,(struct sockaddr*) server_addr,
+	status = connect(fd,(struct sockaddr*) &server_addr,
 	   		 sizeof(server_addr));
 	if(status < 0) error("ERRO ao conectar");
 }
@@ -42,32 +44,35 @@ writeto_socket
 {
 	int fd = *socket_file;
 	char buffer[size];
-	printf("Por favor, entre com a mensagem para o servidor: ");
+	puts("Por favor, entre com a mensagem para o servidor: ");
 	bzero(buffer,size-1);
 	fgets(buffer,size-1,stdin);
 	int n = write(fd,buffer,strlen(buffer));
 	if(n < 0) error("Erro ao escrever no socket");
+}
+	
+void
+readfrom_socket
+(int *socket_file, int size)
+{
+	int status,fd;
+	fd = *socket_file;
+	char buffer[size];
+	bzero(buffer,size-1);
+	status = read(fd,buffer,strlen(buffer));
+	if(status < 0) error("ERRO ao ler do socket");
+	else printf("%s\n",buffer);
 }
 
 int 
 main(int argc, char** argv)
 {
 	if(argc < 3) error("Uso: client hostname porta\n");
-	struct hostent *srv = gethostbyname(argv[1]);
-	struct sockaddr_in socket_addr;
-	int pnu = atoi(argv[2]);
-	int socket_file;
-	if(srv == NULL) error("ERRO, nenhum host encontrado\n");
-	
-	create_socket(&socket_file);
-	build_address(&socket_addr,srv,pnu);
-	connect_socket(&socket_file,&socket_addr);
+	int socket_file = create_socket(&socket_file);
+	struct sockaddr_in address = 
+	build_address(argv[1],atoi(argv[2]));
+	connect_socket(&socket_file,address);
 	writeto_socket(&socket_file,256);
+	readfrom_socket(&socket_file,256);
 	return 0;
-	
-	/*bzero(buffer,256);
-	n = read(sockfd,buffer,255);
-	if(n < 0) error("ERRO ao ler do socket");
-	printf("%s\n",buffer);
-	return 0;*/
 }
